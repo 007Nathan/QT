64f26ecd42819dbb05c4a5f3e21ec5133af9ecf5
007Nathan <34148223+007Nathan@users.noreply.github.com>
2020-04-28 14:08:16 +0800

    无边框窗口FramelessWindowHint实现移动和大小改变
    有两种方案处理这种情况：
    1.自己对mouseMoveEvent,mousePressEvent,mouseReleaseEvent 等事件进行处理。
    2.Qt可以处理windows的消息。重新实现bool winEvent(MSG *message, long *result); QT 5.0以后重写nativeEvent(const QByteArray &eventType, void *message, long *result)

---
+++ MainWindow.h
@@ -0,0 +1,55 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#define PADDING 10
+enum Direction   //拖动方向
+{
+    UP=0,
+    DOWN=1,
+    LEFT,
+    RIGHT,
+    LEFTTOP,
+    LEFTBOTTOM,
+    RIGHTBOTTOM,
+    RIGHTTOP,
+    NONE
+};
+
+
+
+#include <QMainWindow>
+#include <QMouseEvent>
+#include <QWidget>
+#include <windows.h>
+
+#include <windowsx.h>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public:
+    void region(const QPoint &currentGlobalPoint);  //鼠标的位置,改变光标
+protected:
+    //鼠标按下移动及释放事件
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+
+private:
+    QPoint m_movePoint;  //鼠标的位置
+    bool isLeftPressDown;  // 判断左键是否按下
+    Direction dir;        // 窗口大小改变时，记录改变方向
+protected:
+//    bool nativeEvent(const QByteArray &eventType, void *message, long *result);
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
